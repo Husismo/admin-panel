a1ba874b8e98c0a25b70103dd85753e76d170aee
Husismo <43703321+Husismo@users.noreply.github.com>
2022-06-22 15:55:54 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,58 @@
-    function closeNav() {
-        document.getElementById("mySidebar").style.width = "70px";
-        document.getElementById("main").style.marginLeft = "50px";
-      };
+const button = document.querySelector('.sidebar__btn')
+
+const sidebar = document.querySelector('.sidebar')
+
+button.addEventListener('click', () => {
+  sidebar.classList.toggle('toggled')
+})
+var options = {
+  series: [{
+  name: ' Closed deals',
+  data: [49, 100, 99, 170,65,146, 150]
+},],
+  chart: {
+  height: 200,
+  type: 'area',
+  toolbar: false,
+  
+},
+dataLabels: {
+  enabled: false
+},
+stroke: {
+  curve: 'smooth'
+},
+xaxis: {
+  categories: ["1 Dec", "5 Dec", "8 Dec", "12 Dec", "16 Dec", "20 Dec", "31 Dec"]
+},
+tooltip:{
+  x: {
+    show: false,
+},
+z: {
+  formatter: undefined,
+  title: 'Size: '
+},
+}
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+var options = {
+  series: [60, 30, 10],
+  chart: {
+  type: 'donut',
+},
+responsive: [{
+  breakpoint: 480,
+  options: {
+    chart: {
+      width: 100,
+    },
+  }
+}]
+};
+
+var chart = new ApexCharts(document.querySelector("#chart__radiol-bar"), options);
+chart.render();
